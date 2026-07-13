b8b29c814e74a6c2b617486a899c121765fcd499
Ryan Mattson <ryanmattson@outlook.com>
2026-07-13 16:34:33 -0500

    fix enviornment variables

--- .config/hypr/modules/env.lua
+++ .config/hypr/modules/env.lua
@@ -25,7 +25,7 @@ hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
-hl.env("QT_STYLE_OVERRIDE", "qt6ct")
+hl.env("QT_STYLE_OVERRIDE", "kvantum")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 -- NVIDIA
