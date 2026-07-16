5f3745a889dccc135f7cacea4e7c5b85c1f27448
Ryan Mattson <ryanmattson@outlook.com>
2026-07-15 19:13:51 -0500

    Remove Rofi

--- .config/hypr/modules/binds.lua
+++ .config/hypr/modules/binds.lua
@@ -5,7 +5,6 @@
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local menu        = ".config/rofi/app-launcher/launcher.sh"
 
 ---------------------
 ---- KEYBINDINGS ----
@@ -21,7 +20,6 @@ local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close()
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + N", hl.dsp.exec_cmd("swaync-client -t -sw"))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
