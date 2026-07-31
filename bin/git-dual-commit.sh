a3e388aa93abe0dbf143d258a7ea613aac30def3
Ryan Mattson <ryanmattson@outlook.com>
2026-07-31 15:18:38 -0500

    Update dual commit script with -p push flag

--- bin/git-dual-commit.sh
+++ bin/git-dual-commit.sh
@@ -1,5 +1,22 @@
+#!/usr/bin/env bash
 
 # Commits to Laptop and PC branches
+
+push=false
+
+# Parse arguments
+while getopts ":p" opt; do
+    case ${opt} in
+        p )
+            push=true
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" 1>&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
 
 msg="$1"
 
@@ -19,4 +36,10 @@ other_branch="laptop"
 # Apply to the other branch
 git checkout "$other_branch"
 git cherry-pick "$commit_hash"
+if [ "$push" = true ]; then
+    git push origin "$other_branch"
+fi
 git checkout "$current_branch"
+if [ "$push" = true ]; then
+    git push origin "$current_branch"
+fi
