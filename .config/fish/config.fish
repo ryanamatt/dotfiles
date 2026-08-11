9c4ae1acc93170794514e9224b03220876cac4b0
Ryan Mattson <ryanmattson@outlook.com>
2026-08-11 18:26:20 -0500

    Add fasfetch abbreviation ff to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -61,6 +61,8 @@ abbr -a hypr-log 'journalctl --user -u hyprland -f'
 # Swaync
 abbr -a swaync-reload 'swaync-client --reload-config'
 
+abbr -a ff "fastfetch"
+
 # gocryptfs
 abbr -a vo "gocryptfs ~/.vault ~/vault"
 abbr -a vc "fusermount3 -u ~/vault"
@@ -214,4 +216,4 @@ end # end is-interactive
 # Starship
 starship init fish | source
 
-vscode_open_cwd
+# vscode_open_cwd
